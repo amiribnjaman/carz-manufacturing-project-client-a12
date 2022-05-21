490804bd90c25edc80929b705c05f7cbc53f3e0d
amiribnjaman <amiribnjaman@gmail.com>
2022-05-22 02:46:52 +0600

    Signup page designer implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Pages/Auth/Login';
+import Signup from './Pages/Auth/Signup';
 import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
       <Footer />
     </div>
--- src/Pages/Auth/Signup.js
+++ src/Pages/Auth/Signup.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Auth.css';
 
-const Login = () => {
+const Signup = () => {
     return (
-        <div className='login-page md:pt-20'>
+        <div className='signup-page md:pt-10'>
             <div className='w-11/12 mx-auto flex '>
                 <div className='w-1/2'>
                 </div>
                 <div className='w-1/2 '>
                     <div class="p-6 max-w-md grid grid-cols-1 gap-3 bg-white rounded-lg border border-gray-200 shadow-md">
-                        <h2 className='text-xl font-semibold'>Login</h2>
+                        <h2 className='text-xl font-semibold'>Signup</h2>
+                        <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="Your Name" required />
                         <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="Your Email" required />
                         <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="Your Password" required />
-                        <button type="submit" class="text-white mt-4 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">LOGIN</button>
-                        <p className=' text-left'><small className=''>New to carZ? <Link to='/signup' className='text-blue-600'>Signup</Link></small></p>
+                        <button type="submit" class="text-white mt-4 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5">SIGNUP</button>
+                        <p className=' text-left'><small className=''>New to carZ? <Link to='/login' className='text-blue-600'>Login</Link></small></p>
                         <div className='flex items-center px-1 -mt-2'>
                             <div className='w-1/2 bg-black h-[1px]'></div>
                             <div className='px-3'>or</div>
@@ -30,4 +30,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
