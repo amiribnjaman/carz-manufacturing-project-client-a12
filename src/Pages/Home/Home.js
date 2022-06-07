0af91a88ceda735d22413f34919126a5142611ee
amiribnjaman <amiribnjaman@gmail.com>
2022-06-07 15:33:21 +0600

    Site edited a little bit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <>
             <Banner />
-            <InfoSection />
+            {/* <InfoSection /> */}
             <Parts />
             <OurTeam />
             <Reviews />
