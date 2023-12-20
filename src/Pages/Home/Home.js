174bb52cdb1db8dff4db8fca506be7abe49bd17e
amiribnjaman <amiribnjaman@gmail.com>
2023-12-20 19:49:32 +0600

    Contact more info added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy }  from 'react';
 import Banner from './Banner';
 import BusinessSummary from './BusinessSummary';
 import ContactUs from './ContactUs';
+import ShortDescription from "./shortDescription";
 import InfoSection from './InfoSection';
 import OurTeam from './OurTeam';
 import ComponentLoading from '../../Components/componentLoading'
@@ -19,32 +20,33 @@ const ProductCategory = lazy (() => customDelay(import('./ProductCategory')))
 
 const Home = () => {
     return (
-        <>
-            <Banner />
-            {/* <InfoSection /> */}
-            {/* <Parts /> */}
-            <Suspense fallback={<ComponentLoading/>}>
-                <ProductCategory />
-            </Suspense>
-            {/* Lazy loading parts section */}
-            <Suspense fallback={<ComponentLoading/>}>
-                <LatestProduct />
-            </Suspense>
-            <Suspense fallback={<ComponentLoading/>}>
-                <BestSelling />
-            </Suspense>
-            <Suspense fallback={<ComponentLoading/>}>
-                <Products />
-            </Suspense>
-            <OurTeam />
-            {/* Lazy loading parts section */}
-            <Suspense fallback={<LoadingSpinner />}>
-                <Reviews />
-            </Suspense>
-            {/* <BusinessSummary /> */}
-            <ContactUs />
-        </>
-    )
+      <>
+        <Banner />
+        {/* <InfoSection /> */}
+        {/* <Parts /> */}
+        <Suspense fallback={<ComponentLoading />}>
+          <ProductCategory />
+        </Suspense>
+        {/* Lazy loading parts section */}
+        <Suspense fallback={<ComponentLoading />}>
+          <LatestProduct />
+        </Suspense>
+        <Suspense fallback={<ComponentLoading />}>
+          <BestSelling />
+        </Suspense>
+        <Suspense fallback={<ComponentLoading />}>
+          <Products />
+        </Suspense>
+        <OurTeam />
+        {/* Lazy loading parts section */}
+        <Suspense fallback={<LoadingSpinner />}>
+          <Reviews />
+        </Suspense>
+        {/* <BusinessSummary /> */}
+        <ContactUs />
+        <ShortDescription />
+      </>
+    );
 };
 
 const customDelay = (promise) => {
