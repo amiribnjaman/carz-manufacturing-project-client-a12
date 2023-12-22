1fb21985d929c41be37200a1800bf6d840f1c537
amiribnjaman <amiribnjaman@gmail.com>
2023-12-22 21:12:28 +0600

    Fetching and loding spinner issue resolved

--- src/Components/LoadingSpinner.js
+++ src/Components/LoadingSpinner.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { InfinitySpin } from "react-loader-spinner";
+import './CSS/component.css'
 
 const LoadingSpinner = () => {
+
     return (
       <div className="">
-        <div class="flex h-[100vh] w-full text-center items-center justify-center ">
-          <InfinitySpin width="300" className='mx-auto' color="#014E9C" />
+        <div class="loading-spiner flex h-[100vh] w-full text-center items-center justify-center ">
+          <InfinitySpin width="310" color="#014E9C" />
         </div>
       </div>
     );
