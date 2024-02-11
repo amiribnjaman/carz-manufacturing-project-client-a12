f496e5197e719de65b82282fc1d776c464646c1a
amiribnjaman <amiribnjaman@gmail.com>
2024-02-11 13:58:46 +0600

    cart items increment/decrement buttons implemented

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -14,7 +14,7 @@ export default function Cart() {
         <div className="flex gap-4 justify-between mt-3 bg-gray-100 px-2 py-2 rounded">
           <div className="w-1/4">
             <img
-              className="bg-gray-100 h-[120px] rounded-md w-full"
+              className="bg-gray-100 h-[140px] rounded-md w-full"
               src=""
               alt=""
             />
@@ -23,23 +23,28 @@ export default function Cart() {
             <h2 className="text-2xl font-semibold">this is items Title</h2>
             <p>Details</p>
             <h4 className="text-xl font-semibold">280</h4>
+            <div className="flex mt-2 border border-2 border-gray-200 justify-between items-center w-1/3 py-[2px] px-2 rounded-md">
+              <span className="text-xl cursor-pointer">+</span>
+              <span className="text-xl ">1</span>
+              <span className="text-xl cursor-pointer">-</span>
+            </div>
           </div>
           <div className="w-1/4 text-right flex flex-col justify-between">
             <div className="flex ml-auto cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-7 h-7 text-black"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18 18 6M6 6l12 12"
-                  />
-                </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-7 h-7 text-black"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
             </div>
             <div>
               <h5 className="font-semibold">500</h5>
